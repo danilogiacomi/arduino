549568fe544da03bd35b3feb50e2129ce055a4f4
fabrizio <giacomi@netseven.it>
2018-05-25 18:50:48 +0200

    dice altezza acqua: lcd.acqua

--- lcd_acqua/lcd_acqua.ino
+++ lcd_acqua/lcd_acqua.ino
@@ -1,39 +1,41 @@
-
-
-
 #include <LiquidCrystal.h>
-
-
 
 const int rs = 3, en = 4, d4 = 5, d5 = 6, d6 = 7, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 const int w=4;
+int ilValore;
 
 void setup() {
   lcd.begin(16,2);
   Serial.begin(9600);
-
 }
 
 void loop() {
 
-  int ilValore = analogRead(w);
-
-  /*
-  lcd.setCursor(4,0);
-  lcd.print(ilValore);
-  delay(300);
-*/
-
-
-if(ilValore < 300){ 
- lcd.setCursor(0,0);
- lcd.print("                ");
- lcd.setCursor(3,0);
- lcd.print("l'acqua e' bassa");
- delay(500);
- }
-
+  ilValore = analogRead(w);
+  
+  if(ilValore <= 300){ 
+     lcd.setCursor(0,0);
+     lcd.print("                ");
+     lcd.setCursor(3,0);
+     lcd.print("l'acqua e' bassa");
+     delay(500);
+   }
+  
+  if(ilValore > 300 && ilValore < 600){ 
+     lcd.setCursor(0,0);
+     lcd.print("                ");
+     lcd.setCursor(3,0);
+     lcd.print("l'acqua e' media");
+     delay(500);
+   }
+  
+  if(ilValore >= 600){ 
+     lcd.setCursor(0,0);
+     lcd.print("                ");
+     lcd.setCursor(3,0);
+     lcd.print("l'acqua e' alta!");
+     delay(500);
+   }
  
-
 }

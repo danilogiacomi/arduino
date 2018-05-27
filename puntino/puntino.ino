9f8ee430a06eddb4d5bde046f73055859d619400
fabrizio <giacomi@netseven.it>
2018-05-27 20:17:32 +0200

    puntino 1.1 - dry

--- puntino/puntino.ino
+++ puntino/puntino.ino
@@ -8,15 +8,29 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
- 
- for(int j = 0; j < 2; j++) {
-   for(int i = 0; i < 16; i++) {
+void drawPuntino(int x, int y, bool wait = true){
      lcd.clear();
-     lcd.setCursor(i,j);
+     lcd.setCursor(x,y);
      lcd.print(".");
-     delay(500);
-    }
+     if (wait){
+       delay(300);
+     } 
   }
+  
+void loop() {
+
+ 
+  for(int j = 0; j < 2; j++) {
+     for(int i = 0; i < 16; i++) {
+        bool wait = (i != 15) || (j != 1);
+        drawPuntino(i,j, wait);
+     }
+  }
+
+  for(int j = 1; j >= 0; j--) {
+    for(int i = 15; i >= 0; i--) {
+      drawPuntino(i,j);
+     }
+   }
 
 }

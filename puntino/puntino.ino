0185814aeffb5ae9d0da5d7f8ae9485b40902083
fabrizio <giacomi@netseven.it>
2018-05-28 16:21:12 +0200

    puntino definitivo

--- puntino/puntino.ino
+++ puntino/puntino.ino
@@ -10,41 +10,41 @@ void drawPuntino(int x, int y, bool wait = true){
        delay(300);
      } 
   }
-
+void codicePuntino(){
+  for(int k = 0; k <= 2; k++); {
+      for(int j = 0; j < 2; j++) {
+         for(int i = 0; i < 16; i++) {
+            bool wait = (i != 15) || (j != 1);
+            drawPuntino(i,j, wait);
+         }
+      }
+    
+      for(int j = 1; j >= 0; j--) {
+        for(int i = 15; i >= 0; i--) {
+          drawPuntino(i,j);
+         }
+       }
+    }
+  
+   for(int m = 0; m <= 2; m++) {
+      for(int l = 0; l <= 4; l++) {
+         lcd.clear();
+         lcd.setCursor(l,0);
+         lcd.print("puntino");
+         lcd.setCursor(m,1);
+         lcd.print("di Fab e Dan");
+         delay(500);
+      }
+   }
+}
 
 
 void setup() {
   lcd.begin(16,2);
   Serial.begin(9600);
-
-  for(int k = 0; k <= 2; k++); {
-    for(int j = 0; j < 2; j++) {
-       for(int i = 0; i < 16; i++) {
-          bool wait = (i != 15) || (j != 1);
-          drawPuntino(i,j, wait);
-       }
-    }
-  
-    for(int j = 1; j >= 0; j--) {
-      for(int i = 15; i >= 0; i--) {
-        drawPuntino(i,j);
-       }
-     }
-  }
-
- 
- for(int l = 0; l <= 4; l++) {
-  lcd.clear();
-  lcd.setCursor(l,0);
-  lcd.print("puntino");
-  delay(500);
- }
-
- delay(2000);
- lcd.setCursor(2,1);
- lcd.print("di Fab e Dan");
-
+  codicePuntino();
 }
+
   
 void loop() {
 }

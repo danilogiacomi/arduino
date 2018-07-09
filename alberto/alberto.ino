145189c479bf32c6e8fbdaa7288b11af2bfcf79e
fabrizio <giacomi@netseven.it>
2018-07-09 21:58:15 +0200

    varie cose fix alberto etc etc

---
+++ alberto/alberto.ino
@@ -0,0 +1,49 @@
+#include <LiquidCrystal.h>
+const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+int fg = 300;
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.clear();
+}
+
+void loop() {
+  lcd.clear();
+  lcd.setCursor(2,0);
+  lcd.print("mitico!");
+  //lcd.setCursor(2,1);
+  //lcd.print("mitico");
+  // scroll 13 positions (string length) to the left
+  // to move it offscreen left:
+  for (int positionCounter = 0; positionCounter < 13; positionCounter++) {
+    // scroll one position left:
+    lcd.scrollDisplayLeft();
+    // wait a bit:
+    delay(fg);
+  }
+
+  // scroll 29 positions (string length + display length) to the right
+  // to move it offscreen right:
+  for (int positionCounter = 0; positionCounter < 29; positionCounter++) {
+    // scroll one position right:
+    lcd.scrollDisplayRight();
+    // wait a bit:
+    delay(fg);
+  }
+
+  // scroll 16 positions (display length + string length) to the left
+  // to move it back to center:
+  for (int positionCounter = 0; positionCounter < 16; positionCounter++) {
+    // scroll one position left:
+    lcd.scrollDisplayLeft();
+    // wait a bit:
+    delay(fg);
+  }
+
+  // delay at the end of the full loop:
+
+
+}

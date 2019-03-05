5451476bb87a4d2e59d8fd6ced54ee21e1185488
fabrizio <giacomi@netseven.it>
2019-03-05 12:05:20 +0100

    prova (librerie modificate)

---
+++ splash.h
@@ -0,0 +1,15 @@
+#define splash1_width  82
+#define splash1_height 64
+
+const uint8_t PROGMEM splash1_data[] = {
+//senza il logo adafruit
+
+};
+
+#define splash2_width  115
+#define splash2_height  32
+
+const uint8_t PROGMEM splash2_data[] = {
+//senza il logo adafruit (ver più piccola) 
+
+};
